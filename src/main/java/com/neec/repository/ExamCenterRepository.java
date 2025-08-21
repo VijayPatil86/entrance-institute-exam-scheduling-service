3102af74ea320f4759b73fbaef7f39a3c7efbaf3
VijayPatil86 <vijipatil090386@gmail.com>
2025-08-21 19:15:23 +0530

    added custom method existsByCenterName(String centerName)

--- src/main/java/com/neec/repository/ExamCenterRepository.java
+++ src/main/java/com/neec/repository/ExamCenterRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.neec.entity.ExamCenter;
 
 public interface ExamCenterRepository extends JpaRepository<ExamCenter, Long> {
+	boolean existsByCenterName(String centerName);
 }
