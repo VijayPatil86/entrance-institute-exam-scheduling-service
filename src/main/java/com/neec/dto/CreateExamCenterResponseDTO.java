8007ad6a5fb92c256601a25194bc69b0c54f9775
VijayPatil86 <vijipatil090386@gmail.com>
2025-08-21 19:07:23 +0530

    added @Builder annotation

--- src/main/java/com/neec/dto/CreateExamCenterResponseDTO.java
+++ src/main/java/com/neec/dto/CreateExamCenterResponseDTO.java
@@ -2,12 +2,13 @@ package com.neec.dto;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.FieldDefaults;
 
-@AllArgsConstructor @NoArgsConstructor @FieldDefaults(level = AccessLevel.PRIVATE) @Setter @Getter
+@Builder @AllArgsConstructor @NoArgsConstructor @FieldDefaults(level = AccessLevel.PRIVATE) @Setter @Getter
 public class CreateExamCenterResponseDTO {
 	Long centerId;
 	String centerName;
