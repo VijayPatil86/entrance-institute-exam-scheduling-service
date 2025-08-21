26b9e5345261622ca14b3e31fd4e62bf5bd40d2b
VijayPatil86 <vijipatil090386@gmail.com>
2025-08-21 19:06:57 +0530

    completed method to create center

--- src/main/java/com/neec/controller/ExamSchedulingController.java
+++ src/main/java/com/neec/controller/ExamSchedulingController.java
@@ -1,19 +1,32 @@
 package com.neec.controller;
 
-import java.util.Map;
-
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.neec.dto.CreateExamCenterRequestDTO;
+import com.neec.dto.CreateExamCenterResponseDTO;
+import com.neec.service.ExamSchedulingService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @CrossOrigin
 @RequestMapping("/api/v1")
 public class ExamSchedulingController {
+	private ExamSchedulingService examSchedulingService;
+
+	public ExamSchedulingController(ExamSchedulingService examSchedulingService) {
+		this.examSchedulingService = examSchedulingService;
+	}
+
 	@PostMapping("/admin/centers")
-	ResponseEntity<?> createCenter(){
-		return ResponseEntity.ok(Map.of("status", "pass"));
+	ResponseEntity<?> createCenter(@Valid @RequestBody CreateExamCenterRequestDTO dto) {
+		CreateExamCenterResponseDTO createdExamCenter = examSchedulingService.createExamCenter(dto);
+		return ResponseEntity.status(HttpStatus.CREATED.value()).body(createdExamCenter);
 	}
 }
