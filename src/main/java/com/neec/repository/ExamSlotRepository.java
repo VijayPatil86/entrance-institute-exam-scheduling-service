f4940fbdb201da97bdcfaf45d32ca69788baaead
VijayPatil86 <vijipatil090386@gmail.com>
2025-08-22 18:05:23 +0530

    added method to check whether exam slot with same date, start time, end time exists

--- src/main/java/com/neec/repository/ExamSlotRepository.java
+++ src/main/java/com/neec/repository/ExamSlotRepository.java
@@ -1,8 +1,14 @@
 package com.neec.repository;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.neec.entity.ExamSlot;
 
 public interface ExamSlotRepository extends JpaRepository<ExamSlot, Long> {
+	boolean existsByExamCenter_CenterIdAndExamDateAndStartTimeAndEndTime(
+		Long centerId, LocalDate examDate, LocalTime startTime, LocalTime endTime
+	);
 }
