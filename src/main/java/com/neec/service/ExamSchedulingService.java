bb192efe0faf8a986821a80c458af6eef329528d
VijayPatil86 <vijipatil090386@gmail.com>
2025-08-22 18:06:38 +0530

    used method addExamSlot(Long centerId, CreateExamSlotRequest dto)

--- src/main/java/com/neec/service/ExamSchedulingService.java
+++ src/main/java/com/neec/service/ExamSchedulingService.java
@@ -2,11 +2,12 @@ package com.neec.service;
 
 import com.neec.dto.CreateExamCenterRequestDTO;
 import com.neec.dto.CreateExamCenterResponseDTO;
+import com.neec.dto.CreateExamSlotRequest;
 
 public interface ExamSchedulingService {
 	// Admin Operations
 	CreateExamCenterResponseDTO createExamCenter(CreateExamCenterRequestDTO dto);
-	//void addExamSlot(Long centerId, ExamSlotDTO dto);
+	void addExamSlot(Long centerId, CreateExamSlotRequest dto);
 	//List<ExamCenterDTO> getAllCenters();
 
 	// Applicant Operations
