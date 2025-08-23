e078a90e9e79e7ce0af6ca53f78e363891941727
VijayPatil86 <vijipatil090386@gmail.com>
2025-08-23 17:14:58 +0530

    added request handler method addExamSlot()

--- src/main/java/com/neec/controller/ExamSchedulingController.java
+++ src/main/java/com/neec/controller/ExamSchedulingController.java
@@ -1,8 +1,11 @@
 package com.neec.controller;
 
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.neec.dto.CreateExamCenterRequestDTO;
 import com.neec.dto.CreateExamCenterResponseDTO;
+import com.neec.dto.CreateExamSlotRequest;
 import com.neec.service.ExamSchedulingService;
 
 import jakarta.validation.Valid;
@@ -28,5 +32,12 @@ public class ExamSchedulingController {
 	ResponseEntity<?> createCenter(@Valid @RequestBody CreateExamCenterRequestDTO dto) {
 		CreateExamCenterResponseDTO createdExamCenter = examSchedulingService.createExamCenter(dto);
 		return ResponseEntity.status(HttpStatus.CREATED.value()).body(createdExamCenter);
+	}
+
+	@PostMapping("/admin/centers/{centerId}/slots")
+	ResponseEntity<?> addExamSlot(@PathVariable(name = "centerId") Long centerId,
+			@Valid @RequestBody CreateExamSlotRequest slotRequest){
+		examSchedulingService.addExamSlot(centerId, slotRequest);
+		return ResponseEntity.status(HttpStatus.CREATED.value()).body(Map.of("status", "Exam Slot added successfully"));
 	}
 }
