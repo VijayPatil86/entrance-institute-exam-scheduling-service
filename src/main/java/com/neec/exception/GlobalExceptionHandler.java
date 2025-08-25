a7f940454aad7f3cda4828cc0a8ef1ba67945c10
VijayPatil86 <vijipatil090386@gmail.com>
2025-08-25 19:13:55 +0530

    added exception handling for request parameter city in getting available slots for given city

--- src/main/java/com/neec/exception/GlobalExceptionHandler.java
+++ src/main/java/com/neec/exception/GlobalExceptionHandler.java
@@ -1,13 +1,19 @@
 package com.neec.exception;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.HandlerMethodValidationException;
+
+import jakarta.validation.ConstraintViolationException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -32,5 +38,19 @@ public class GlobalExceptionHandler {
 			errors.put(error.getObjectName(), error.getDefaultMessage())
 		);
 		return ResponseEntity.badRequest().body(errors);
+	}
+
+	@ExceptionHandler(exception = {MissingServletRequestParameterException.class})
+	public ResponseEntity<?> handleMissingServletRequestParameterException(MissingServletRequestParameterException ex) {
+		return ResponseEntity.badRequest().body(Map.of("errors", ex.getMessage()));
+	}
+
+	@ExceptionHandler(exception = {ConstraintViolationException.class})
+	public ResponseEntity<?> handleConstraintViolationException(ConstraintViolationException ex) {
+		List<String> errors = ex.getConstraintViolations()
+		        .stream()
+		        .map(violation -> violation.getMessage())
+		        .collect(Collectors.toList());
+	    return ResponseEntity.badRequest().body(Map.of("errors", errors));
 	}
 }
