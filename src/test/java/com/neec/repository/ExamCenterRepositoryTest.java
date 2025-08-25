1d08df809715da3d3e3c78e6891e479276a482d1
VijayPatil86 <vijipatil090386@gmail.com>
2025-08-25 12:05:41 +0530

    added test to verify city names are unique, in natural order

--- src/test/java/com/neec/repository/ExamCenterRepositoryTest.java
+++ src/test/java/com/neec/repository/ExamCenterRepositoryTest.java
@@ -1,6 +1,11 @@
 package com.neec.repository;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,5 +41,33 @@ public class ExamCenterRepositoryTest {
 		ExamCenter savedExamCenter = examCenterRepository.save(examCenter);
 		boolean isExamCenterExists = examCenterRepository.existsByCenterName("A B College");
 		assertTrue(isExamCenterExists);
+	}
+
+	@Test
+	void test_findAllCities_Returns_DistinctCityNames_NaturalOrder() {
+		List<ExamCenter> listExamCentersToSave = sampleExamCenters();
+		examCenterRepository.saveAll(listExamCentersToSave);
+		List<String> listCityNames = examCenterRepository.findAllCities();
+		long countDistinctCityNames = listCityNames.stream().distinct().count();
+		assertEquals(countDistinctCityNames, listCityNames.size(), "duplicate city names found");
+		List<String> copyOfCityNames = new ArrayList<>(listCityNames);
+		Collections.sort(copyOfCityNames);
+		assertEquals(copyOfCityNames, listCityNames, "city names are not in natural order");
+	}
+
+	List<ExamCenter> sampleExamCenters() {
+		return List.of(
+				ExamCenter.builder().centerName("A B College").addressLine("A. B. Road").city("Pune")
+						.state("Maharashtra").pinCode("147258").contactPerson("Mr. R.K. Sane")
+						.contactPhone("9876543210").build(),
+				ExamCenter.builder().centerName("C D College").addressLine("C. D. Road").city("Mumbai")
+						.state("Maharashtra").pinCode("369875").contactPerson("Mr. R.K. Mane")
+						.contactPhone("8876543210").build(),
+				ExamCenter.builder().centerName("E F College").addressLine("E. F. Road").city("Pune")
+						.state("Maharashtra").pinCode("100236").contactPerson("Mr. R.K. More")
+						.contactPhone("7876543210").build(),
+				ExamCenter.builder().centerName("G H College").addressLine("G. H. Road").city("Thane")
+						.state("Maharashtra").pinCode("556644").contactPerson("Mr. R.K. Kore")
+						.contactPhone("9876543211").build());
 	}
 }
