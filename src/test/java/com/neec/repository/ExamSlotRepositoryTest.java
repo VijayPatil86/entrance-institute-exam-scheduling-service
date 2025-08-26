d67974e520bf885c78ed06c16bca584be7a1ecf6
VijayPatil86 <vijipatil090386@gmail.com>
2025-08-26 18:58:37 +0530

    added test case to test existsByExamCenter_CenterIdAndExamDateAndStartTimeAndEndTime()

--- src/test/java/com/neec/repository/ExamSlotRepositoryTest.java
+++ src/test/java/com/neec/repository/ExamSlotRepositoryTest.java
@@ -1,6 +1,7 @@
 package com.neec.repository;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.time.LocalDate;
@@ -24,6 +25,41 @@ public class ExamSlotRepositoryTest {
 	private ExamSlotRepository examSlotRepository;
 	@Autowired
 	private ExamCenterRepository examCenterRepository;
+
+	@DirtiesContext
+	@Test
+	void test_existsByExamCenter_CenterIdAndExamDateAndStartTimeAndEndTime() {
+		ExamCenter examCenter_Pune = ExamCenter.builder().centerName("A B College").addressLine("A. B. Road").city("Pune")
+				.state("Maharashtra").pinCode("147258").contactPerson("Mr. R.K. Sane")
+				.contactPhone("9876543210").build();
+		ExamCenter savedExamCenter_Pune = examCenterRepository.save(examCenter_Pune);
+		ExamSlot examSlot_Pune = ExamSlot.builder()
+				.examCenter(savedExamCenter_Pune)
+				.examDate(LocalDate.of(2025, 01, 01))
+				.startTime(LocalTime.of(10, 0))
+				.endTime(LocalTime.of(10, 30))
+				.totalSeats(10)
+				.bookedSeats(5)
+				.build();
+		examSlotRepository.save(examSlot_Pune);
+		boolean bExists =
+				examSlotRepository.existsByExamCenter_CenterIdAndExamDateAndStartTimeAndEndTime(
+						savedExamCenter_Pune.getCenterId(),
+						LocalDate.of(2025, 01, 01),
+						LocalTime.of(10, 0),
+						LocalTime.of(10, 30)
+					);
+		assertTrue(bExists, "Exam slot should exist for the given center, date, and time");
+
+		boolean notExists =
+				examSlotRepository.existsByExamCenter_CenterIdAndExamDateAndStartTimeAndEndTime(
+						savedExamCenter_Pune.getCenterId(),
+						LocalDate.of(2025, 01, 02),
+						LocalTime.of(10, 0),
+						LocalTime.of(10, 30)
+					);
+		assertFalse(notExists, "Exam slot should not exist for a different date");
+	}
 
 	@DirtiesContext
 	@Test
