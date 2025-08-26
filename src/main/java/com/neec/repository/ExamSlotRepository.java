10a71f9a1b8355c0b3bbd7a3fa403a6c15ee9722
VijayPatil86 <vijipatil090386@gmail.com>
2025-08-26 18:17:24 +0530

    added method to get slot record with lock

--- src/main/java/com/neec/repository/ExamSlotRepository.java
+++ src/main/java/com/neec/repository/ExamSlotRepository.java
@@ -3,11 +3,15 @@ package com.neec.repository;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 
 import com.neec.entity.ExamSlot;
+
+import jakarta.persistence.LockModeType;
 
 public interface ExamSlotRepository extends JpaRepository<ExamSlot, Long> {
 	boolean existsByExamCenter_CenterIdAndExamDateAndStartTimeAndEndTime(
@@ -15,4 +19,6 @@ public interface ExamSlotRepository extends JpaRepository<ExamSlot, Long> {
 	);
 	@Query(value = "SELECT es FROM ExamSlot es JOIN FETCH es.examCenter ec WHERE ec.city = :city AND es.bookedSeats < es.totalSeats")
 	List<ExamSlot> findAvailableSlotsByCity(String city);
+	@Lock(LockModeType.PESSIMISTIC_WRITE)
+	Optional<ExamSlot> findWithLockingBySlotId(Long slotId);
 }
